364002937313c7444bc48938b5a7bd093a459d61
Pessera <73839942+Pessera@users.noreply.github.com>
2020-11-04 16:10:34 +0300

    Update pm.lua

--- pm.lua
+++ pm.lua
@@ -6,8 +6,8 @@ encoding.default = 'CP1251'
 local utf8 = encoding.UTF8
 
 local tag = '[{AF7AF1}Test{40EB3D}{FFFFFF}] '
-local version_scr = 3
-local version_text = "1.30"
+local version_scr = 1
+local version_text = "1.00"
 local update = false
 local path_update = getWorkingDirectory() .. "/update.ini"
 local url_update = "https://raw.githubusercontent.com/Pessera/vary_bad_scripts/main/update.ini"
